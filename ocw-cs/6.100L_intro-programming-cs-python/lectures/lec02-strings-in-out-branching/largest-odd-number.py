77d43dcb8e6f82a6dac0daf72a782496d98d6ed3
João Victor <jotav.lim4@gmail.com>
2025-10-01 12:47:19 -0300

    Exemplifica o uso de valores provisórios que mudam de acordo com condições testadas ao longo da execução.

---
+++ ocw-cs/6.100L_intro-programming-cs-python/lectures/lec02-strings-in-out-branching/largest-odd-number.py
@@ -0,0 +1,21 @@
+x = 70
+y = 10
+z = 20
+
+# só imprimimos o valor mínimo quando não há impares entre os números testados
+# então inicializamos a resposta com o menor entre eles
+answer = min(x, y, z) 
+
+# se existir um número impar, então ele será nossa resposta atual.
+# verificamos se existe outro impar, caso exista comparamos com o atual
+# e atualizamos a resposta se for necessário.
+
+if x % 2 != 0:
+    answer = x
+if y % 2 != 0 and y > answer:
+    answer = y
+if z % 2 != 0 and z > answer:
+    answer = z
+
+print(answer)
+
