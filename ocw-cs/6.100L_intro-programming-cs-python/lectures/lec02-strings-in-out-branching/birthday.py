f9f67361324ca494f28024ce3ad7dac035aa0389
João Victor <jotav.lim4@gmail.com>
2025-10-01 12:47:42 -0300

    Exemplifica o uso de fatiamento de string em problemas com input

---
+++ ocw-cs/6.100L_intro-programming-cs-python/lectures/lec02-strings-in-out-branching/birthday.py
@@ -0,0 +1,4 @@
+birthday = input("type your birthday (on the follow way mm/dd/yyyy): ")
+# usamos o fatiamento de string para criar uma substring 
+# com o valor conveniente para o problema
+print(f"you were born in the year {birthday[6:]}")
