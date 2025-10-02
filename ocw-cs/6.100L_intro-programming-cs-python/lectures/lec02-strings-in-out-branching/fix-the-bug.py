541d371f8d7208a7ef42b3395baaa89f3bdc6a58
João Victor <jotav.lim4@gmail.com>
2025-10-02 09:29:12 -0300

    Destaca a importância da identação em python

---
+++ ocw-cs/6.100L_intro-programming-cs-python/lectures/lec02-strings-in-out-branching/fix-the-bug.py
@@ -0,0 +1,7 @@
+x = int(input("enter a number for x: "))
+y = int(input("enter a different number for y: "))
+
+if x == y:
+    print(x, "is the same as", y)
+    print("these are equal!") # basta identar essa linha de código de forma que ela pertença ao if
+
