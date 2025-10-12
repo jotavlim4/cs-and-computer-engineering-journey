5b7f8370bff529671dbbe57b8d4e136228c05e15
João Victor <jotav.lim4@gmail.com>
2025-10-12 15:06:31 -0300

    includindo enunciado do finger exercise

--- 6.100l-intro-programming-cs-python/lectures/lec02-strings-in-out-branching/birthday.py
+++ 6.100l-intro-programming-cs-python/lectures/lec02-strings-in-out-branching/birthday.py
@@ -1,3 +1,7 @@
+""" Write code that asks the user to enter their
+birthday in the form mm/dd/yyyy, and then prints a string of the
+form ‘You were born in the year yyyy.’ """
+
 birthday = input("type your birthday (on the follow way mm/dd/yyyy): ")
 # usamos o fatiamento de string para criar uma substring 
 # com o valor conveniente para o problema
--- 6.100l-intro-programming-cs-python/lectures/lec02-strings-in-out-branching/largest-odd-number.py
+++ 6.100l-intro-programming-cs-python/lectures/lec02-strings-in-out-branching/largest-odd-number.py
@@ -1,3 +1,7 @@
+"""Write a program that examines three variables—
+x, y, and z—and prints the largest odd number among them. If none
+of them are odd, it should print the smallest value of the three. """
+
 x = 70
 y = 10
 z = 20
